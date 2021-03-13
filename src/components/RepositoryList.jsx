74a9c929d57fa19beabc47f1318a7b3d37e39329
Caroline Carvalho <carvalhocaroline111@gmail.com>
2021-03-12 22:20:29 -0300

    Propriedades no React

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,35 +1,21 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+  name: 'Github Explorer',
+  description: 'Repositório do primeiro módulo do Ignite',
+  link: 'https://github.com/Carol111/ignite-react-01-github-explorer'
+}
+
 export function RepositoryList() {
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
-
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
-
-        <li>
-          <strong>unform</strong>
-          <p>Forms in React</p>
-
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem />
+        <RepositoryItem />
+        <RepositoryItem />
       </ul>
     </section>
   )
